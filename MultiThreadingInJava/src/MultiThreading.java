9e3ce6700b6882aeeb91e5242326cfb565cce824
Srishtilamichhane <59720998+Srishtilamichhane@users.noreply.github.com>
2022-02-02 06:24:19 +0545

    Multithreading

---
+++ MultiThreadingInJava/src/MultiThreading.java
@@ -0,0 +1,53 @@
+
+ class ThreadCount extends Thread {
+	
+	ThreadCount()
+	{
+		super("overriding Thread class");
+		System.out.println("New Thread created"+this);
+		start();
+	}
+	
+	public void run()
+	{
+		try {
+			for (int i=0;i<10;i++)
+			{
+				System.out.println("New Thread created"+this);
+				
+				Thread.sleep(1500);
+			}
+		}
+		catch(InterruptedException e)
+		{
+			System.out.println("Currently executing thread is interrupted");	
+		}
+		
+		System.out.println("Currently executing thread run is terminated");
+	}
+	
+}
+
+public class MultiThreading
+{
+	public static void main(String []args)
+	{
+		ThreadCount c= new ThreadCount();
+		
+		
+		try {
+			while(c.isAlive())
+			{
+				System.out.println("Main Method Thread will be alive,until its child Thread says alive");
+			}
+			Thread.sleep(2500);
+		}
+		catch(InterruptedException e)
+		{
+ 			    System.out.println("Main method thread is interrupted");
+		}
+		
+		
+		System.out.println("Main methods thread run is terminited ");
+	}
+}
